45e83e335b2380b31440e8e7eb4cbc98356d654e
zk <zk@DESKTOP-G6NKT2J>
2016-10-20 17:26:11 +0800

    All finish

--- MvcTest/Areas/SPA/Controllers/MainController.cs
+++ MvcTest/Areas/SPA/Controllers/MainController.cs
@@ -14,6 +14,7 @@ namespace MvcTest.Areas.SPA.Controllers
     public class MainController : Controller
     {
         // GET: SPA/Main
+        [Authorize]
         public ActionResult Index()
         {
             MainViewModel v = new MainViewModel();
